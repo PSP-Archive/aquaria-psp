d0e83db1b412dad5cecedf613170d6d14c723f22
achurch <devnull@localhost>
2010-06-19 08:09:07 +0900

    Ensure input is properly grabbed on a second Ctrl+G.
    This patch makes a slight fix to David Gow's input grab patch
    (icculus.org r20:634f22109eb0) so that input is regrabbed immediately
    on a second Ctrl+G, rather than leaving it ungrabbed until a window
    focus change occurs.

--- BBGE/Core.cpp
+++ BBGE/Core.cpp
@@ -3355,7 +3355,7 @@ void Core::pollEvents()
 				{
 					// toggle mouse grab with the magic hotkey.
 					grabInputOnReentry = (grabInputOnReentry)?0:-1;
-					setReentryInputGrab(0);
+					setReentryInputGrab(1);
 				}
 				else if (_hasFocus)
 				{
