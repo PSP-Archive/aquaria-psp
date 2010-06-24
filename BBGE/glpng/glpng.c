e8ae972d84838602e6c657f98f9472c731432655
achurch <devnull@localhost>
2010-06-24 16:25:59 +0900

    Avoid unused-result warnings in external libraries.
    This patch avoids an unused result warning in the glpng library included
    with the Aquaria source code.

--- BBGE/glpng/glpng.c
+++ BBGE/glpng/glpng.c
@@ -269,7 +269,7 @@ int APIENTRY pngLoadRawF(FILE *fp, pngRawInfo *pinfo) {
 
 	if (pinfo == NULL) return 0;
 
-	fread(header, 1, 8, fp);
+	if (fread(header, 1, 8, fp) != 8) return 0;
 	if (!png_check_sig(header, 8)) return 0;
 
 	png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
@@ -373,7 +373,7 @@ int APIENTRY pngLoadF(FILE *fp, int mipmap, int trans, pngInfo *pinfo) {
 
 	png_uint_32 i;
 
-	fread(header, 1, 8, fp);
+	if (fread(header, 1, 8, fp) != 8) return 0;
 	if (!png_check_sig(header, 8)) return 0;
 
 	png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
