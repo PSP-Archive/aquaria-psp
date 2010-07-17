2e48105d8657d71cf362dfce9979d80f38e3bc62
achurch <devnull@localhost>
2010-07-17 12:50:35 +0900

    Fix a floating-point variable improperly declared as int.
    This patch fixes an oversight in the icon shrinking patch
    (r263:b6a62e2afa90) which left a variable intended to be floating-point
    declared as integer, resulting in a somewhat jerky "heartbeat" animation
    for the warp and save icons.

--- Aquaria/MiniMapRender.cpp
+++ Aquaria/MiniMapRender.cpp
@@ -615,7 +615,7 @@ void MiniMapRender::onRender()
 					if (render)
 					{
 						glTranslatef(miniMapPos.x, miniMapPos.y, 0);
-						int sz = iconSize * iconScale;
+						const float sz = iconSize * iconScale;
 
 						glBegin(GL_QUADS);
 							glTexCoord2f(0, 1);
